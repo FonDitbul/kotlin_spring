06d0cc3e924fd7d74b1d43ac5db344a6805530ed
FonDitbul <niki7084@naver.com>
2023-12-05 23:06:23 +0900

    Entities 생성

---
+++ src/main/kotlin/com/example/kotlin_spring/blog/entity/Entities.kt
@@ -0,0 +1,10 @@
+package com.example.kotlin_spring.blog.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+class Wordcount(
+    @Id val word: String,
+    val cnt: Int = 0
+)
